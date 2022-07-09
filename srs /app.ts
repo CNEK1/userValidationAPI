04bac527529729be740ec5277419f2db47989ec4
CNEK1 <darkghostspain1@gmail.com>
2022-07-09 23:58:50 +0200

    first commit

---
+++ srs /app.ts
@@ -0,0 +1,37 @@
+import express, { Express } from 'express';
+import { Server } from 'http';
+import { ExceptionFilter } from './errors/exception.filter';
+import { LoggerService } from './logger/logger.service';
+import { UserController } from './users /user.controller';
+
+export class App {
+    app: Express;
+    port: number;
+    server: Server;
+    logger: LoggerService;
+    userController: UserController;
+    exceptionFilter: ExceptionFilter;
+
+    constructor(logger: LoggerService, userController: UserController, exceptionFilter: ExceptionFilter) {
+        this.app = express();
+        this.port = 8000;
+        this.logger = logger;
+        this.userController = userController;
+        this.exceptionFilter = exceptionFilter;
+    }
+
+    useRoutes() {
+        this.app.use('/users', this.userController.router);
+    }
+
+    useExceptionFilter() {
+        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+    }
+
+    public async init() {
+        this.useRoutes();
+        this.useExceptionFilter();
+        this.server = this.app.listen(this.port);
+        this.logger.log(`Server:  http://localhost:${this.port}`);
+    }
+}
---
+++ srs /users /user.controller.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { LoggerService } from '../logger/logger.service';
+
+export class UserController extends BaseController {
+    constructor(logger: LoggerService) {
+        super(logger);
+        this.bindRoutes([{ path: '/register', method: 'post', func: this.register }]);
+        this.bindRoutes([{ path: '/login', method: 'post', func: this.login }]);
+    }
+
+    login(req: Request, res: Response, next: NextFunction) {
+        //Emulation Error
+        next(new HTTPError(401, 'Error of auth', 'login'));
+    }
+    register(req: Request, res: Response, next: NextFunction) {
+        this.ok(res, 'register');
+    }
+}
