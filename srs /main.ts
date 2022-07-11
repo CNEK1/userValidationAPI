42e80e48f86366a393242a98f1efa1502aa74c6b
CNEK1 <darkghostspain1@gmail.com>
2022-07-11 18:19:39 +0200

    Adding Infersify (Dypendency Injection)

--- srs /main.ts
+++ srs /main.ts
@@ -1,12 +1,29 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
 import { App } from './app';
 import { ExceptionFilter } from './errors/exception.filter';
+import { IExceptionFilter } from './errors/exception.filter.interface';
+import { ILogger } from './logger/logger.interface';
 import { LoggerService } from './logger/logger.service';
+import { TYPES } from './types';
 import { UserController } from './users /user.controller';
+import { IUserController } from './users /user.inteface';
 
-const bootstrap = async () => {
-    const logger = new LoggerService();
-    const app = new App(logger, new UserController(logger), new ExceptionFilter(logger));
-    await app.init();
+// const logger = new LoggerService();
+// const app = new App(logger, new UserController(logger), new ExceptionFilter(logger));
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+    bind<ILogger>(TYPES.ILogger).to(LoggerService);
+    bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+    bind<IUserController>(TYPES.UserController).to(UserController);
+    bind<App>(TYPES.Application).to(App);
+});
+
+const bootstrap = () => {
+    const appContainer = new Container();
+    appContainer.load(appBindings);
+    const app = appContainer.get<App>(TYPES.Application);
+    app.init();
+    return { appContainer, app };
 };
 
-bootstrap();
+export const { app, appContainer } = bootstrap();
--- srs /users /user.controller.ts
+++ srs /users /user.controller.ts
@@ -1,11 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
+import { inject, injectable } from 'inversify';
 import { BaseController } from '../common/base.controller';
 import { HTTPError } from '../errors/http-error.class';
-import { LoggerService } from '../logger/logger.service';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+import 'reflect-metadata';
+import { IUserController } from './user.inteface';
 
-export class UserController extends BaseController {
-    constructor(logger: LoggerService) {
-        super(logger);
+@injectable()
+export class UserController extends BaseController implements IUserController {
+    constructor(@inject(TYPES.ILogger) private loggerSrc: ILogger) {
+        super(loggerSrc);
         this.bindRoutes([{ path: '/register', method: 'post', func: this.register }]);
         this.bindRoutes([{ path: '/login', method: 'post', func: this.login }]);
     }
